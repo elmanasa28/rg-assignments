2c36e8d73b0fe9f1d88df0ab7882de9a9e39be95
elmanasa28 <104914185+elmanasa28@users.noreply.github.com>
2024-07-19 17:27:03 +0530

    Create DateTimeExample.java

---
+++ DateTimeExample.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+public class DateTimeExample {
+
+    public static void main(String[] args) {
+       
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        
+        System.out.println("Current Date and Time: " + currentDateTime);
+    }
+}
